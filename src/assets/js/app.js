99280cc777f772154ed77666f78b4bc81cc2ac97
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-11-23 17:51:01 +0300

    new

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -693,6 +693,21 @@ if (filterButtons.length && closeButtons.length) {
     })
 }
 
+
+var acc = document.querySelectorAll(".panel .panel__title");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+
 });
 
 
